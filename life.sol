3f5bbc35f0e00d71c3cddb8c44d9620dfc3b4515
pnrmhcl <pmihcioglu@gmail.com>
2022-03-03 14:02:39 +0300

    add life cycle

---
+++ life.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.6.0;
+
+contract Lifecycle {
+
+    enum Stage{Infant, Toddler, Child, Teen, Adult, Old}
+
+    function getStage(uint months) public pure returns (Stage){
+        if(months > 0 && months <= 12)
+            return Stage.Infant;
+        else if(months > 12 && months <= 24)
+            return Stage.Toddler;
+        else if(months > 24 && months <= 155)
+            return Stage.Child;
+        else if(months > 156 && months <= 228)
+            return Stage.Teen;
+        else if(months > 228 && months <= 720)
+            return Stage.Adult;
+        else
+            return Stage.Old;
+    }
+}
